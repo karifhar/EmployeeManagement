b966ddd3480994db8b63fb1f91b5bbfc6b1798d6
Karifhar <rifqahanif.a@gmail.com>
2024-06-30 11:57:52 +0700

    first init

--- EmployeeManagement.Contracts/obj/Debug/net8.0/EmployeeManagement.Contracts.AssemblyInfo.cs
+++ EmployeeManagement.Contracts/obj/Debug/net8.0/EmployeeManagement.Contracts.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeManagement.Contracts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbf7033ea8ea9e123d2486918e7946d547ab4323")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeManagement.Contracts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeManagement.Contracts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
